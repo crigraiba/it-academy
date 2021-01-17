5c772be9e1eccef03cb84696661e2f4b588d6e68
Cristina <crigraiba@gmail.com>
2021-01-17 19:58:06 +0100

    Mòdul 1 Nivell 3

---
+++ M01_N3.java
@@ -0,0 +1,24 @@
+
+/* Rotar un array sense utilitzar
+   un array auxiliar ni llibreries.
+*/
+
+public class M01_N3 {
+
+	public static void main(String[] args) {
+		
+		int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
+		for (int i = 0; i < arr.length/2; i++) {
+			int aux = arr[i];
+			arr[i] = arr[arr.length-1-i];
+			arr[arr.length-1-i] = aux;
+		}
+
+		for (int t: arr) {
+			System.out.println(t);
+		}
+		
+	}
+
+}
