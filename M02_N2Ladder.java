3e7196a5195de0c6fec3ecf33fea805121ae7e3c
Cristina <crigraiba@gmail.com>
2021-01-17 19:58:25 +0100

    Mòdul 2 Nivell 2

---
+++ M02_N2Ladder.java
@@ -0,0 +1,22 @@
+
+// ESCALA DE NÚMEROS
+
+import java.util.Scanner;
+
+public class M02_N2Ladder {
+
+	public static void main(String[] args) {
+		System.out.print("Introdueix l'alçada: ");
+		Scanner input = new Scanner(System.in);
+		int height = input.nextInt();
+		input.close();
+		
+		for (int step = 1; step <= height; step++) {
+			for (int num = 1; num <= step; num++) {
+				System.out.print(num);
+			}
+			System.out.print("\n");
+		} // num < step	System.out.println(step);
+	}
+
+}
---
+++ M02_N2Pyramid.java
@@ -0,0 +1,28 @@
+
+// PIRÀMIDE INVERTIDA
+
+import java.util.Scanner;
+
+public class M02_N2Pyramid {
+
+	public static void main(String[] args) {
+		System.out.print("Introdueix l'alçada: ");
+		Scanner input = new Scanner(System.in);
+		int height = input.nextInt();
+		input.close();
+		
+		int total = 0;
+		for (int step = height*2-1; step >= 1; step -= 2) {
+			for (int whitespace = 0; whitespace != total; whitespace++) {
+				System.out.print(" ");
+			}
+			total++;
+			
+			for (int num = 1; num <= step; num++) {
+				System.out.print("*");
+			}
+			System.out.print("\n");
+		}
+	}
+
+}
