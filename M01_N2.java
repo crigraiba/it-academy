5512fb572f38d33d63d202d3ce1f0ef3b30e5c3b
Cristina <crigraiba@gmail.com>
2021-01-17 19:58:01 +0100

    Mòdul 1 Nivell 2

---
+++ M01_N2.java
@@ -0,0 +1,22 @@
+
+// Type Casting: double -> int, float, String
+
+public class M01_N2 {
+
+	public static void main(String[] args) {
+		double numDouble = 0.1234;
+		
+		int numInt;
+		float numFloat;
+		String numString;
+		
+		numInt = (int)numDouble;
+		numFloat = (float)numDouble;
+		numString=Double.toString(numDouble);
+		
+		System.out.println(numInt);
+		System.out.println(numFloat);
+		System.out.println(numString);
+	}
+
+}
