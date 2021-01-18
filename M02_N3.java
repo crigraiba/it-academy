2aed0969385791ed487bf1d0ff61e47d1018759b
Cristina <crigraiba@gmail.com>
2021-01-18 18:16:03 +0100

    Mòdul 2 Nivell 3 Millorat

--- M02_N3.java
+++ M02_N3.java
@@ -3,35 +3,53 @@
 
 public class M02_N3 {
 
-	public static void main(String[] args) throws InterruptedException {
+	public static void main(String[] args) {
 		
 		// 00:00:00
 		
+		String watch;
 		for (int hour = 0; hour <= 23; hour++) {
 			for (int minute = 0; minute <= 59; minute++) {
 				for (int second = 0; second <= 59; second++) {
 					if (hour < 10) {
-						System.out.print("0"+hour);
+						watch = "0"+hour;
 					} else {
-						System.out.print(hour);
+						watch = Integer.toString(hour);
 					}
 					if (minute < 10) {
-						System.out.print(":0"+minute);
+						watch += ":0"+minute;
 					} else {
-						System.out.print(":"+minute);
+						watch += ":"+minute;
 					}
 					if (second < 10) {
-						System.out.print(":0"+second);
+						watch += ":0"+second;
 					} else {
-						System.out.print(":"+second);
+						watch += ":"+second;
 					}
-					Thread.sleep(1000); // Unhandled exception type InterruptedException
+					System.out.print(watch);
+					try {
+						Thread.sleep(1000); 
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					} // Unhandled exception type InterruptedException
 					System.out.print("\b\b\b\b\b\b\b\b");
 					if (hour == 23 && minute == 59 && second == 59) {
 						hour = -1;
 					}
 				}
 			}
+			
+			/* Alternativament:
+			 * import java.time.LocalTime;
+			 * import java.time.format.DateTimeFormatter;
+			 * 
+			 * while (true) {
+			 *     LocalTime time = LocalTime.now();
+			 *     String timeToShow = time.format(DateTimeFormatter.ISO_LOCAL_TIME);
+			 *     String[] timeArray = timeToShow.split("\\.");
+			 *     System.out.println(timeArray[0]);
+			 * }
+			 */
 		}
 	}
 
