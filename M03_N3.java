eb390aa382a4fcc6704fee75e1c778f282dde2f8
Cristina <crigraiba@gmail.com>
2021-01-19 18:14:18 +0100

    Mòdul 3 Nivell 3 Sense recursivitat

--- M03_N3.java
+++ M03_N3.java
@@ -15,6 +15,11 @@ public class M03_N3 {
 		} while (num < 0);
 		sc.close();
 		
+		// v1(num);
+		v2_SenseRecursivitat(num);
+	}
+	
+	static void v1(int num) {
 		for (int i = 0; i < num; i++) {
 			System.out.println(Fibonacci(i));
 		}
@@ -25,6 +30,22 @@ public class M03_N3 {
 			return n;
 		} else {
 			return Fibonacci(n-1) + Fibonacci(n-2);
+		}
+	}
+	
+	static void v2_SenseRecursivitat(int num) {
+		int f1 = 1, f2 = 0, aux;
+		for (int i = 0; i < num; i++) {
+			if (i == 0) {
+				System.out.println(f2);
+			} else if (i == 1) {
+				System.out.println(f1);
+			} else {
+				aux = f1;
+				f1 += f2;
+				f2 = aux;
+				System.out.println(f1);
+			}
 		}
 	}
 }
