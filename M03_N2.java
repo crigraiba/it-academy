ea19d450787cb9efc4a0dbcd8b9fbb04f38cd314
Cristina <crigraiba@gmail.com>
2021-01-19 17:12:59 +0100

    Mòdul 3 Nivell 2

---
+++ M03_N2.java
@@ -0,0 +1,33 @@
+
+import java.util.Scanner;
+
+public class M03_N2 {
+
+	public static void main(String[] args) {
+		double[][] notesAlumnes = new double[5][3];
+		double suma;
+		Scanner sc = new Scanner(System.in);
+		
+		for (int i = 0; i < 5; i++) {
+			System.out.println("Alumne "+(i+1)+":");
+			for (int j = 0; j < 3; j++) {
+				do {
+					System.out.print("Nota "+(j+1)+" = ");
+					notesAlumnes[i][j] = sc.nextDouble();
+				} while (notesAlumnes[i][j] < 0 || notesAlumnes[i][j] > 10);
+			}
+			
+			suma = 0;
+			for (int j = 0; j < 3; j++) {
+				suma += notesAlumnes[i][j];
+			}
+			if (suma/3 < 5) {
+				System.out.println("Suspès\n");
+			} else {
+				System.out.println("Aprovat\n");
+			}
+		}
+		sc.close();
+	}
+
+}
