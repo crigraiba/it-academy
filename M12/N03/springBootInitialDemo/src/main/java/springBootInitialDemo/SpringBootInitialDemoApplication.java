4faca92de2eac8380dc2205f633dd30fef63265e
Cristina <crigraiba@gmail.com>
2021-04-23 23:25:59 +0200

    Mòdul 12 Nivell 3

---
+++ M12/N03/springBootInitialDemo/src/main/java/springBootInitialDemo/SpringBootInitialDemoApplication.java
@@ -0,0 +1,13 @@
+package springBootInitialDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootInitialDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootInitialDemoApplication.class, args);
+	}
+
+}
---
+++ M12/N03/springBootInitialDemo/src/test/java/springBootInitialDemo/SpringBootInitialDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package springBootInitialDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootInitialDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
