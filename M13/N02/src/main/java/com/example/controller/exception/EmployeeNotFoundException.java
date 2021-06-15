b4393fd4284ff9c0b4e0242ce5bd4da71055d66b
Cristina <crigraiba@gmail.com>
2021-06-15 14:07:37 +0200

    Mòdul 13 Nivell 2

--- M13/N02/src/main/java/com/example/controller/exception/EmployeeNotFoundException.java
+++ M13/N02/src/main/java/com/example/controller/exception/EmployeeNotFoundException.java
@@ -1,9 +1,5 @@
 package com.example.controller.exception;
 
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.http.HttpStatus;
-
-@ResponseStatus(value = HttpStatus.NOT_FOUND) // 404
 public class EmployeeNotFoundException extends RuntimeException {
 	
 	private static final long serialVersionUID = 1L;
