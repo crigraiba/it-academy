6a8df78613ace79ea775768e81934be5c11088ba
Cristina <crigraiba@gmail.com>
2021-06-15 13:42:30 +0200

    Mòdul 13 Nivell 1

--- M13/N01/src/main/java/com/example/controller/EmployeeController.java
+++ M13/N01/src/main/java/com/example/controller/EmployeeController.java
@@ -1,105 +1,81 @@
 package com.example.controller;
 
 import java.util.ArrayList;
+import java.util.Map;
 
 import javax.validation.Valid;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.http.HttpStatus;
-import org.springframework.ui.Model;
 
 import com.example.repository.InMemoryEmployeeRepository;
 import com.example.domain.Employee;
 import com.example.domain.EJob;
+import com.example.controller.exception.EmployeeNotFoundException;
 
-@Controller
+@RestController
 @RequestMapping("/employees")
 public class EmployeeController {
 	
-	@Autowired
-	InMemoryEmployeeRepository repository;
+	InMemoryEmployeeRepository repository = new InMemoryEmployeeRepository();
 	
 	// Lectura:
 	@GetMapping
-	@ResponseBody
-	/*public String read(Model model) {
-		model.addAttribute("employees", repository.read());
-		return "index";
-	}*/
 	public ArrayList<Employee> read() {
-		return repository.read();
+		ArrayList<Employee> employees = repository.read();
+		return employees;
 	}
 	
 	@GetMapping("/{id}")
-	@ResponseBody
-	/*public String readById(Model model, RedirectAttributes ra, @PathVariable int id) {
-		ra.addFlashAttribute("employee", repository.readById(id));
-		return "redirect:/employees";
-	}*/
 	public Employee readById(@PathVariable int id) {
-		return repository.readById(id);
+		Employee employee = repository.readById(id).orElseThrow(() -> new EmployeeNotFoundException(id));
+		return employee;
 	}
 	
 	// Creació:
 	@PostMapping
-	@ResponseStatus(HttpStatus.CREATED) // 201
-	/*public String create(Model model, @Valid @RequestBody Employee employee) {
-		repository.create(new Employee("creació", EJob.ADVOCAT));
-		return "redirect:/employees";
-	}*/
-	public void create() {
-		repository.create(new Employee("creació", EJob.ADVOCAT));
+	public void create(@Valid @RequestParam String name, @Valid @RequestParam EJob job) {
+		Employee employee = new Employee(name, job);
+		
+		repository.create(employee);
 	}
 	
 	// Eliminació:
 	@DeleteMapping("/{id}")
-	@ResponseStatus(HttpStatus.NO_CONTENT) // 204
-	/*public String delete(Model model, @PathVariable int id) {
-		repository.delete(id);
-		return "redirect:/employees";
-	}*/
 	public void delete(@PathVariable int id) {
+		// Comprovem que l'empleat existeixi:
+		this.readById(id);
+		
 		repository.delete(id);
 	}
 	
 	// Actualització:
 	@PutMapping("/{id}")
-	@ResponseStatus(HttpStatus.OK) // 200
-	/*public String update(Model model, @PathVariable int id, @Valid @RequestBody Employee employee) {
-		repository.update(new Employee("actualització", EJob.ADVOCAT));
-		return "redirect:/employees";
-	}*/
-	public void update(@PathVariable int id) {
-		Employee employee = new Employee();
+	public void update(@PathVariable int id, @Valid @RequestBody Map<String, String> json) {
+		// Comprovem que l'empleat existeixi:
+		this.readById(id);
 		
+		String name = json.get("name");
+		EJob job = EJob.valueOf(json.get("job"));
+		
+		Employee employee = new Employee(name, job);
 		employee.setId(id);
-		employee.setName("actualització");
-		employee.setJob(EJob.ADVOCAT.name());
-		employee.setSalary(EJob.ADVOCAT.getSalary());
 		
 		repository.update(employee);
 	}
 	
 	// Filtratge:
 	@GetMapping(params = "job")
-	@ResponseBody
-	/*public String filterByJob(Model model, @Valid @RequestParam EJob job) {
-		model.addAttribute("employees", repository.filterByJob(job.name()));
-		return "index";
-	}*/
-	public ArrayList<Employee> filterByJob(@RequestParam EJob job) {
-		return repository.filterByJob(job.name());
+	public ArrayList<Employee> filterByJob(@Valid @RequestParam EJob job) {
+		ArrayList<Employee> employees = repository.filterByJob(job.name());
+		return employees;
 	}
 	
 }
--- M13/N01/src/main/java/com/example/domain/Employee.java
+++ M13/N01/src/main/java/com/example/domain/Employee.java
@@ -3,8 +3,8 @@ package com.example.domain;
 public class Employee {
 	
 	private int id;
-	private static int count = 1;
-	private String name, job;
+	private String name;
+	private String job;
 	private double salary;
 	
 	// Mètodes constructors:
@@ -12,10 +12,16 @@ public class Employee {
 	}
 	
 	public Employee(String name, EJob job) {
-		this.id = count++;
 		this.name = name;
 		this.job = job.name();
 		this.salary = job.getSalary();
+	}
+	
+	public Employee(int id, String name, String job, double salary) {
+		this.id = id;
+		this.name = name;
+		this.job = job;
+		this.salary = salary;
 	}
 	
 	// Mètodes setter:
@@ -50,6 +56,12 @@ public class Employee {
 	
 	public double getSalary() {
 		return salary;
+	}
+	
+	// Altres mètodes:
+	@Override
+	public String toString() {
+		return "ID=" + id + ", Nom=" + name + ", Feina=" + job + ", Salari=" + salary;
 	}
 	
 }
