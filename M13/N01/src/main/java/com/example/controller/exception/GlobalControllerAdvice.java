93f025026c1bbf09b92dec1cce64069df5b109f9
Cristina <crigraiba@gmail.com>
2021-06-15 14:07:22 +0200

    Mòdul 13 Nivell 1

--- M13/N01/src/main/java/com/example/controller/exception/GlobalControllerAdvice.java
+++ M13/N01/src/main/java/com/example/controller/exception/GlobalControllerAdvice.java
@@ -1,19 +1,22 @@
 package com.example.controller.exception;
 
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 @RestControllerAdvice
 public class GlobalControllerAdvice {
 	
 	@ExceptionHandler(EmployeeNotFoundException.class)
+	@ResponseStatus(value = HttpStatus.NOT_FOUND) // 404
 	public String handleException(EmployeeNotFoundException e) {
-		return e.getClass() + " " + e.getMessage();
+		return e.getClass().getSimpleName() + ": " + e.getMessage();
 	}
 	
 	@ExceptionHandler(Exception.class)
 	public String handleException(Exception e) {
-		return e.getClass() + " " + e.getMessage();
+		return e.getClass().getSimpleName() + ": " + e.getMessage();
 	}
 	
 }
