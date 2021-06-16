42811215984cb8dbd0242e72df92addb400cf14f
Cristina <crigraiba@gmail.com>
2021-06-16 18:34:56 +0200

    Mòdul 14 Nivell 2

--- M14/N02/src/main/java/com/example/controller/ShopRestController.java
+++ M14/N02/src/main/java/com/example/controller/ShopRestController.java
@@ -2,6 +2,8 @@ package com.example.controller;
 
 import java.util.ArrayList;
 
+import javax.validation.Valid;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -39,7 +41,7 @@ public class ShopRestController {
 	}
 	
 	@PostMapping
-	public String create(@RequestParam String name, @RequestParam int capacity) {
+	public String create(@Valid @RequestParam String name, @RequestParam int capacity) {
 		Shop shop = new Shop(name, capacity);
 		shopRepository.save(shop);
 		
